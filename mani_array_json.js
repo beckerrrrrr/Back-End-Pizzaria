5a6189c09718cb24178a6009c0a0ee92185b1623
beckerrrrrr <123740236+beckerrrrrr@users.noreply.github.com>
2023-12-13 17:43:48 -0300

    Add files via upload

---
+++ mani_array_json.js
@@ -0,0 +1,15 @@
+const pizzaria=require('./pizzaria')
+
+const getListaClientes = () => {
+    let usuarios = pizzaria.usuario.usuario;
+    let usuariosArray = [];
+
+    clientes.forEach((cliente) => { 
+
+      
+
+        usuarioArray.push(clienteDados);
+    });
+
+    return usuarioArray; 
+};
